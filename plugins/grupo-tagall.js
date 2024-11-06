f23962e3c9afff91f6f7fbe97128c1792500a61d
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-11-06 02:25:52 +0100

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `@𝖈𝖊𝖔𝖌𝖊𝖗𝖎𝖕𝖎𝖚𝖒.𝖉𝖟𝖓🫦 ${pesan}`
-let teks = `*🙂‍↕𝙈𝙞𝙘𝙝𝙞𝘽𝙤𝙩 𝙡𝙤𝙨 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙡𝙖𝙣𝙩𝙖𝙨🙂‍↔*\n𝘔𝘪𝘤𝘩𝘪 𝘦𝘭 𝘮𝘦𝘫𝘰𝘳 𝘣𝘰𝘵🥇\n${oi}\n\n*🌤️𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
+let oi = `@its_txx.ff ${pesan}`
+let teks = `*ʚ 🩷 Llenen lista o cupo 🩷 ɞ*\n${oi}\n\n*🌤️𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
 for (let mem of participants) {
-teks += `☀️✨≋ @${mem.id.split('@')[0]}\n`}
-teks += `MichiBot`
+teks += `ʚ🩷ɞ @${mem.id.split('@')[0]}\n`}
+teks += `MiauBot`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
